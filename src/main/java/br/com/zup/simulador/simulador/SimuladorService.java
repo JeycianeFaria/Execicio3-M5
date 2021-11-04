5f6fffc962d103b651345475d93d55e2f09c51fa
Jeyciane Faria <jeycianedeveloper@gmail.com>
2021-11-04 13:45:59 -0300

    Criar método para calcular investimento

--- src/main/java/br/com/zup/simulador/simulador/SimuladorService.java
+++ src/main/java/br/com/zup/simulador/simulador/SimuladorService.java
@@ -1,8 +1,28 @@
 package br.com.zup.simulador.simulador;
 
+import br.com.zup.simulador.simulador.dtos.RetornoDTO;
 import br.com.zup.simulador.simulador.dtos.RiscoDTO;
 import br.com.zup.simulador.simulador.dtos.SimuladorDTO;
 
 public class SimuladorService {
+
+    private RetornoDTO retornoSimulacao = new RetornoDTO();
+
+    public double calcularInvestimento(SimuladorDTO dadosSimulador){
+        double valorTotalLucro = dadosSimulador.getValorInvestido();
+
+        for (int contador = 0; contador < dadosSimulador.getMesesDeAplicacao(); contador ++ ){
+            if (dadosSimulador.getRisco() == RiscoDTO.ALTO){
+                valorTotalLucro = valorTotalLucro + (valorTotalLucro * 0.025);
+
+            }else if (dadosSimulador.getRisco() == RiscoDTO.MEDIO){
+                valorTotalLucro = valorTotalLucro + (valorTotalLucro * 0.015);
+            }else if (dadosSimulador.getRisco() == RiscoDTO.BAIXO){
+                valorTotalLucro = valorTotalLucro + (valorTotalLucro * 0.005);
+            }
+        }
+
+        return valorTotalLucro;
+    }
 
 }
